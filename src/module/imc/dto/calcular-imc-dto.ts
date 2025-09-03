7eb48d8e37bd55a543e7a853cf726d3e3a567745
Alejo <alejodm12345@gmail.com>
2025-09-03 16:52:29 -0300

    Propiedades del DTO

--- src/module/imc/dto/calcular-imc-dto.ts
+++ src/module/imc/dto/calcular-imc-dto.ts
@@ -1,11 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, Max, Min } from 'class-validator';
 
 export class CalcularImcDto {
+  @ApiProperty({
+    example: 1.75,
+    description: 'Altura de la persona en metros (0.1 - 3)',
+  })
   @IsNumber()
   @Min(0.1) // Altura mínima razonable
   @Max(3) // Altura máxima razonable
   altura: number;
 
+  @ApiProperty({
+    example: 70,
+    description: 'Peso de la persona en kg (1 - 500)',
+  })
   @IsNumber()
   @Min(1) // Peso mínimo razonable
   @Max(500) // Peso máximo razonable
