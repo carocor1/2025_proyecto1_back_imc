1774264f72a367ce37f181bfd125e33333b1438b
Alejo <alejodm12345@gmail.com>
2025-09-03 16:53:18 -0300

    DTO para la respuesta del cálculo imc

---
+++ src/module/imc/dto/respuesta-imc-dto.ts
@@ -0,0 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ImcResponse {
+  @ApiProperty({ example: 22.86, description: 'Índice de Masa Corporal calculado' })
+  imc: number;
+
+  @ApiProperty({ example: 'Normal', description: 'Categoría según el IMC' })
+  categoria: string;
+}
