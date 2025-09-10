da0b29bc6a31db7737e807ffe4bb0f6135e04971
MartinBeccereca <martinbeccereca@gmail.com>
2025-09-10 11:49:32 -0300

    Configuración de BD en la nube usando Aiven.io

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { ImcHistorialModule } from './imc-historial/imc-historial.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Module({
   imports: [
@@ -16,9 +18,13 @@ import { UsersModule } from './users/users.module';
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
+      ssl: {
+        ca: fs.readFileSync(path.resolve(__dirname, '..', '..', '2025_proyecto1_back_imc', 'src', 'config', 'ca.pem')),
+        rejectUnauthorized: true, // Requerido para verificar el certificado
+      },
     }),
     ImcModule,
     ImcHistorialModule,
